397b5d9f57ad04e4e1cece8a6028268ce3b20ef7
Chris Gurrin <cgurrindev@gmail.com>
2024-08-03 20:07:46 +1000

    use html-react-parser

--- src/App.tsx
+++ src/App.tsx
@@ -3,21 +3,21 @@ import { FileUpload } from './FileUpload';
 import './App.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPrint } from '@fortawesome/free-solid-svg-icons';
-import { faQuestion } from '@fortawesome/free-solid-svg-icons/faQuestion';
-import { Help } from './Help';
+import parse from 'html-react-parser';
 
 const App = () => {
-    const [htmlContent, setHtmlContent] = useState('');
-    const [isHelpOpen, setIsHelpOpen] = useState(false);
+    const [htmlContent, setHtmlContent] = useState(undefined);
 
     return (
         <div className="bg-zinc-700 w-full min-h-screen absolute">
             <div
                 id="toolbar"
-                className="flex gap-4 py-2 px-12 gap-6 sticky top-0 z-40"
+                className="flex gap-4 py-2 px-12 gap-6 sticky top-0 z-50"
             >
                 <FileUpload
-                    onFileUpload={(html: string) => setHtmlContent(html)}
+                    onFileUpload={(html: string) => {
+                        setHtmlContent(parse(html));
+                    }}
                 />
                 |
                 <button
@@ -27,24 +27,14 @@ const App = () => {
                     <FontAwesomeIcon icon={faPrint} className="text-zinc-600" />
                     Print
                 </button>
-                |
-                <button
-                    onClick={() => setIsHelpOpen(true)}
-                    className="bg-zinc-200 px-4 flex gap-2 items-center justify-center border-2 hover:border-sky-600"
-                >
-                    <FontAwesomeIcon
-                        icon={faQuestion}
-                        className="text-zinc-600"
-                    />
-                    Help
-                </button>
             </div>
             <br />
-            <div
+            {/* <div
                 className="z-10"
                 dangerouslySetInnerHTML={{ __html: htmlContent }}
-            ></div>
-            <Help isOpen={isHelpOpen} setIsOpen={setIsHelpOpen} />
+            ></div> */}
+            {htmlContent ?? ''}
+            {}
         </div>
     );
 };
